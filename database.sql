4da8bc10c4aeb9f26efe0d0ce7bc78c39e8aa461
fabricien-web <rakotoarimananafabricien@gmail.com>
2019-08-07 11:33:06 +0300

    first php

---
+++ database.sql
@@ -0,0 +1,9 @@
+
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(110) NOT NULL,
+  `age` int(110) NOT NULL,
+  `email` varchar(110) NOT NULL,
+  PRIMARY KEY  (`id`)
+);
